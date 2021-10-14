f18dd31eba763bb13d6fb8285fa3a4947becb48e
Fernanda Rizky Prabowo <89926535+FernandaRizky27@users.noreply.github.com>
2021-10-15 00:35:26 +0700

    Add files via upload

---
+++ Chapter 5/Latsol5.py
@@ -0,0 +1,10 @@
+#menampilkan data diagram horizontal
+print('Grafik diagram batang horizontal mahasiswa PTIK 2021')
+lk = int(input('Masukkan banyak mahasiswa laki-laki :'))
+pr =  int(input("Masukkan banyak mahasiswa perempuan :"))
+#Tampilan
+print("GRAFIK DIAGRAM BATANG HORIZONTAL MAHASISWA PTIK 2021")
+data1= ("*" * lk)
+data2=("*" * pr)
+print(data1, lk)
+print(data2, pr)
